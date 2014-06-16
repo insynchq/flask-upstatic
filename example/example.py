5993c87c9df22a4165c9833000d2a6de546cb20b
Mark Steve Samson <hello@marksteve.com>
2014-06-16 14:12:14 +0800

    Rewrite
    * Remove support for packages
    * Simplified compile and upload flow

--- example/example.py
+++ example/example.py
@@ -1,24 +1,23 @@
 import logging
+import os
+
+from flask import Flask, render_template
+from flask_upstatic import Upstatic
 
 logger = logging.getLogger('flask_upstatic')
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
 
-from flask import Flask, render_template
-from flask_upstatic import Upstatic
-
 
 if __name__ == '__main__':
   app = Flask(__name__)
   app.config.update(
-    UPSTATIC_S3_ACCESS_KEY_ID=raw_input('S3 Access Key ID: '),
-    UPSTATIC_S3_SECRET_ACCESS_KEY=raw_input('S3 Secret Access Key: '),
-    UPSTATIC_S3_BUCKET_NAME='flask_upstatic',
+    UPSTATIC_S3_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID'),
+    UPSTATIC_S3_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+    UPSTATIC_S3_BUCKET_NAME='flask_upstatic_example',
   )
   upstatic = Upstatic(app)
-  upstatic.add_root('home', 'static')
-
-  upstatic.upload_all('home')
+  upstatic.upload()
 
   app.add_template_global(upstatic.url_for)
 
@@ -26,5 +25,4 @@ if __name__ == '__main__':
   def index():
     return render_template('index.html')
 
-  app.run()
-
+  app.run(host='0.0.0.0')
--- flask_upstatic.py
+++ flask_upstatic.py
@@ -4,19 +4,14 @@ import logging
 import mimetypes
 import os
 import re
+import shutil
 from functools import partial
 from hashlib import md5
 from tempfile import mkdtemp
 
-import pkg_resources
-
 from boto.s3.connection import OrdinaryCallingFormat, S3Connection
 from flask import url_for as _url_for
-from flask import current_app, request
-from werkzeug import routing
-
-logger = logging.getLogger(__name__)
-
+from flask import request
 
 CSS_URL_PATTERN = re.compile('url\((?:\'|")?([^\'"\)]+)(?:\'|")?\)')
 
@@ -24,21 +19,15 @@ CSS_URL_PATTERN = re.compile('url\((?:\'|")?([^\'"\)]+)(?:\'|")?\)')
 class Upstatic(object):
 
   _compiled_paths = {}
-  _roots = {}
 
   def __init__(self, app=None):
     if app:
       self.init_app(app)
-    self._app = app
-
-  @property
-  def app(self):
-    return self._app or current_app
 
   @property
   def use_local(self):
-    return (self.app.config.get('UPSTATIC_DEBUG_USE_LOCAL', True) and
-            self.app.config.get('DEBUG'))
+    return (self._app.config.get('UPSTATIC_DEBUG_USE_LOCAL', True) and
+            self._app.config.get('DEBUG'))
 
   def _walk(self, path):
     for dirpath, _dirnames, filenames in os.walk(path):
@@ -53,274 +42,247 @@ class Upstatic(object):
     mimetype = mimetypes.guess_type(filename)[0]
     return mimetype and mimetype.startswith('image')
 
-  def _get_compiled_path(self, path, is_gzip, protocol, compiled_data=None):
-    compiled_data = compiled_data or self._compile(path, is_gzip, protocol)
-    key = "%s\0%s\0%s" % (path, is_gzip, protocol)
-    compiled_path = self._compiled_paths.get(key)
-    if not compiled_path:
-      basename = os.path.basename(path)
-      md5 = self._get_md5(compiled_data)
-      if self._is_image(basename):
-        compiled_path = '/'.join(('c', md5[:1], md5[1:7], basename))
-      elif is_gzip:
-        compiled_path = '/'.join(('c', md5[:1], md5[1:7], 'gz', basename))
-      else:
-        compiled_path = '/'.join(('c', md5[:1], md5[1:7], 'raw', basename))
-      self._compiled_paths[key] = compiled_path
-    return compiled_path
+  def _is_css(self, filename):
+    mimetype = mimetypes.guess_type(filename)[0]
+    return mimetype and mimetype == 'text/css'
 
-  def _css_replace_url(self, path, is_gzip, protocol, match):
-    url = match.group(1).rsplit('#', 1)[0].rsplit('?', 1)[0]
-    if url.startswith('http'):  # absolute url
-      return url
-    elif url.startswith('/'):  # from root
-      return url
-    else:  # relative to the CSS
-      _path = os.path.join(os.path.dirname(path), url)
-      _compiled_path = self._get_compiled_path(
-        _path,
-        is_gzip,
-        protocol,
-      )
-      return "url('%s')" % self._build_url(_compiled_path, protocol)
+  def _key(self, *args):
+    return "\0".join(map(str, args))
 
-  def _compile(self, path, is_gzip, protocol):
+  def _get_compiled_path(self, path, is_gzip):
+    # Get file contents
     try:
       with open(path) as f:
-        contents = f.read()
+        data = f.read()
     except IOError:
-      return ''
-    ext = os.path.splitext(path)[1].lower()
-    if ext == '.css':
-      css_replace_url = partial(
-        self._css_replace_url,
-        path,
-        is_gzip,
-        protocol,
+      raise
+
+    md5 = self._get_md5(data)
+    basename = os.path.basename(path)
+
+    # Get compiled path
+    compression = 'gz' if is_gzip else 'raw'
+    compiled_path = os.path.join('c', md5[:1], md5[1:7], compression, basename)
+
+    return compiled_path
+
+  def _css_replace_url(self, is_gzip, match):
+    path = match.group(1).rsplit('#', 1)[0].rsplit('?', 1)[0]
+    if path.startswith('http'):  # absolute url
+      return path
+    elif path.startswith('/'):  # from root
+      return path
+    else:  # relative to the CSS
+      if self._is_image(path):
+        is_gzip = False
+      compiled_path = self._compiled_paths.get(
+        self._key(
+          path,
+          is_gzip,
+        ),
       )
-      return CSS_URL_PATTERN.sub(css_replace_url, contents)
-    else:
-      return contents
+      return "url('%s')" % self._build_url(compiled_path)
 
-  def _compile_path(self, path, protocol='http'):
-    tmpdir = mkdtemp()
+  def _fix_css(self, compiled_data, is_gzip):
+    sub = partial(self._css_replace_url, is_gzip)
+    return CSS_URL_PATTERN.sub(sub, compiled_data)
 
-    logger.info("Compile dir: %r", tmpdir)
+  def compile(self):
+    self._logger.info("Compiling...")
 
     compiled = set()
-    compiled_gz = set()
 
-    for filename in self._walk(path):
+    images = set()
+    css = set()
+
+    gzipped = set()
+    raw = set()
+
+    # Collect files
+    for path in self._walk(self._app.static_folder):
+
+      # Get relative path
+      path = os.path.relpath(path)
 
       for is_gzip in True, False:
 
-        if is_gzip and self._is_image(filename):  # Don't gzip images
-          continue
-
-        compiled_data = self._compile(filename, is_gzip, protocol)
-
-        compiled_filename = self._get_compiled_path(
-          filename,
-          is_gzip,
-          protocol,
-          compiled_data=compiled_data,
-        )
-
-        if compiled_filename in compiled | compiled_gz:
-          logger.warn(
-            "Already compiled: %r -> %r",
-            filename,
-            compiled_filename,
-          )
-          continue
-
-        compiled_dirname = os.path.dirname(compiled_filename)
+        # Get compiled path
+        compiled_path = self._get_compiled_path(path, is_gzip)
 
         # Create directories
+        compiled_dirname = os.path.dirname(compiled_path)
         try:
-          os.makedirs(os.path.join(tmpdir, compiled_dirname))
+          os.makedirs(os.path.join(self._tmpdir, compiled_dirname))
         except OSError:
           pass
 
-        # Compile
-        try:
-          output = os.path.join(tmpdir, compiled_filename)
+        # Collect images
+        # NOTE: Images aren't gzipped
+        if self._is_image(path):
           if is_gzip:
-            f = gzip.open(output, 'wb')
-            f.write(compiled_data)
-            f.close()
+            continue
           else:
-            with open(output, 'wb') as f:
-              f.write(compiled_data)
+            images.add(compiled_path)
 
-        except Exception:
-          logger.error(
-            "Failed to compile: %r -> %r",
-            filename,
-            compiled_filename,
-          )
-          raise
+        # Collect css files
+        if self._is_css(path):
+          css.add(compiled_path)
 
+        # Collect to be gzipped files
+        if is_gzip and not self._is_image(path):
+          gzipped.add(compiled_path)
         else:
-          if is_gzip:
-            compiled_gz.add(compiled_filename)
-          else:
-            compiled.add(compiled_filename)
-          logger.info(
-            "Compiled: %r -> %r",
-            filename,
-            compiled_filename,
-          )
+          raw.add(compiled_path)
 
-    logger.info("%s compiled and %s gzip'd.", len(compiled), len(compiled_gz))
+        # Copy to compile path
+        shutil.copy(path, os.path.join(self._tmpdir, compiled_path))
+        compiled.add(compiled_path)
 
-    return tmpdir, compiled, compiled_gz
+        # Memoize
+        self._compiled_paths[self._key(
+          # NOTE: Path is relative to static folder
+          os.path.relpath(path, self._app.static_folder),
+          is_gzip,
+        )] = compiled_path
 
-  def _upload(self, tmpdir, filename, bucket, headers, key_prefix):
+        self._logger.debug('Compiled: %r -> %r', path, compiled_path)
+
+    # Fix CSS paths
+    for compiled_path in css:
+      output = os.path.join(self._tmpdir, compiled_path)
+      with open(output, 'rb') as f:
+        data = f.read()
+      is_gzip = '/gz/' in compiled_path
+      with open(output, 'wb') as f:
+        f.write(self._fix_css(data, is_gzip))
+
+    # Gzip files
+    for compiled_path in gzipped:
+      output = os.path.join(self._tmpdir, compiled_path)
+      with open(output, 'rb') as f:
+        data = f.read()
+      fgz = gzip.open(output, 'wb')
+      fgz.write(data)
+      fgz.close()
+
+    compiled_count = dict(
+      all=len(compiled),
+      css=len(css),
+      images=len(images),
+      gzip=len(gzipped),
+      raw=len(raw),
+    )
+
+    self._logger.info("Compiled: %r", compiled_count)
+
+    return compiled
+
+  def _upload(self, path, bucket, headers, key_prefix, overwrite):
     try:
-      key_name = key_prefix + filename
+      key_name = key_prefix + path
       key = bucket.get_key(key_name)
-      if key:
-        logger.info("Already exists: %r", filename)
+      if key and not overwrite:
+        self._logger.info("Already exists: %r", path)
         return False
       else:
         key = bucket.new_key(key_name)
         key.set_contents_from_filename(
-          os.path.join(tmpdir, filename),
+          os.path.join(self._tmpdir, path),
           headers=headers,
         )
         key.set_acl('public-read')
-        logger.info("Uploaded: %r", filename)
+        self._logger.debug("Uploaded: %r", path)
         return True
 
     except Exception:
-      logger.error("Failed to upload: %r", filename)
+      self._logger.error("Failed to upload: %r", path)
       raise
 
-  def _build_url(self, path, protocol='http'):
-    url = self._memo.get(path)
-    if not url:
-      path = self._key_prefix + path
-      if self._cdn_host:
-        url = "%s://%s/%s" % (protocol, self._cdn_host, path)
-      else:
-        url = self._calling_format.build_url_base(
-          self._conn,
-          protocol,
-          S3Connection.DefaultHost,
-          self._bucket_name,
-          path,
-        )
-      self._memo[path] = url
-    return url
+  def _build_url(self, path):
+    path = self._key_prefix + path
+    if self._cdn_host:
+      return "%s://%s/%s" % (self._scheme, self._cdn_host, path)
+    else:
+      return self._calling_format.build_url_base(
+        self._conn,
+        self._scheme,
+        S3Connection.DefaultHost,
+        self._bucket_name,
+        path,
+      )
 
   def init_app(self, app):
+    self._app = app
+
     self._access_key_id = app.config['UPSTATIC_S3_ACCESS_KEY_ID']
     self._secret_access_key = app.config['UPSTATIC_S3_SECRET_ACCESS_KEY']
     self._bucket_name = app.config['UPSTATIC_S3_BUCKET_NAME']
+
     self._key_prefix = app.config.get('UPSTATIC_S3_KEY_PREFIX', '')
     self._cdn_host = app.config.get('UPSTATIC_S3_CDN_HOST')
+    self._scheme = app.config.get('UPSTATIC_SCHEME', 'http')
+
     self._calling_format = OrdinaryCallingFormat()
-    self._memo = {}
+
     self._conn = S3Connection(self._access_key_id, self._secret_access_key)
+    self._tmpdir = mkdtemp()
+
+    self._logger = logging.getLogger(__name__) or logging.root
+
+    if not self.use_local:
+      self.compile()
 
   def url_for(self, *args, **kwargs):
-    root = self._roots.get(kwargs.pop('root', None))
-
-    if not (args[0].endswith('static') and root):
+    if self.use_local or not args[0].endswith('static'):
       return _url_for(*args, **kwargs)
 
+    # Normalize path
     path = kwargs['filename'].strip('/')
 
-    if self.use_local:
-      root_url = root.get('url')
-      if root_url:
-        url = os.path.join(root_url, path)
-      else:
-        url = _url_for(*args, **kwargs)
-      return url
-
     # Get headers info
-    is_gzip = 'gzip' in request.headers.get('Accept-Encoding', '')
-    protocol = request.headers.get('X-Forwarded-Proto', 'https')
+    is_gzip = (
+      'gzip' in request.headers.get('Accept-Encoding', '') and
+      not self._is_image(path)
+    )
 
     # Get compiled path
-    compiled_path = self._get_compiled_path(
-      os.path.join(root['path'], path),
-      is_gzip,
-      protocol,
+    compiled_path = self._compiled_paths[self._key(path, is_gzip)]
+
+    return self._build_url(compiled_path)
+
+  def upload(self, overwrite=False):
+    self._logger.info("Uploading...")
+
+    bucket = self._conn.create_bucket(self._bucket_name)
+    key_prefix = self._key_prefix
+    headers = {
+      'Cache-Control': 'max-age=31556926',  # 1 year
+    }
+    uploaded = dict(
+      raw=0,
+      gzip=0,
     )
+    for compiled_path in self._compiled_paths.values():
+      is_gzip = '/gz/' in compiled_path
 
-    return self._build_url(compiled_path, protocol)
-
-  def get_data_path(self, base_dir, package_name=None):
-    if not package_name:
-      return base_dir
-    requirement = pkg_resources.Requirement.parse(package_name)
-    return pkg_resources.resource_filename(requirement, base_dir)
-
-  def add_root(self, name, static_folder, package_name=None, root_url=None):
-    self._roots[name] = dict(
-      path=self.get_data_path(static_folder, package_name),
-      url=root_url,
-    )
-
-  def upload_all(self, *roots, **kwargs):
-    protocol = kwargs.get('protocol', 'http')
-
-    for root in roots:
-      path = self._roots[root]['path']
-
-      # Compile
-      tmpdir, compiled, compiled_gz = self._compile_path(path, protocol)
-
-      s3_conn = S3Connection(
-        self.app.config['UPSTATIC_S3_ACCESS_KEY_ID'],
-        self.app.config['UPSTATIC_S3_SECRET_ACCESS_KEY'],
+      if is_gzip:
+        headers['Content-Encoding'] = 'gzip'
+      else:
+        headers.pop('Content-Encoding', None)
+      headers['Content-Type'] = (
+        mimetypes.guess_type(compiled_path)[0] or
+        'application/octet-stream'
       )
-      bucket = s3_conn.create_bucket(self.app.config['UPSTATIC_S3_BUCKET_NAME'])
-      key_prefix = self.app.config.get('UPSTATIC_S3_KEY_PREFIX') or ''
 
-      headers = {
-        'Cache-Control': 'max-age=31556926',  # 1 year
-      }
-
-      # Upload
-      files_uploaded = 0
-
-      for compiled_filename in compiled:
-        headers['Content-Type'] = (
-          mimetypes.guess_type(compiled_filename)[0] or
-          'application/octet-stream'
-        )
-        files_uploaded += self._upload(
-          tmpdir,
-          compiled_filename,
+      if self._upload(
+          compiled_path,
           bucket,
           headers,
           key_prefix,
-        )
+          overwrite,
+        ):
+        uploaded['raw'] += not is_gzip
+        uploaded['gzip'] += is_gzip
 
-      # Upload gzip'd
-      files_gz_uploaded = 0
+    self._logger.info("Uploaded: %r", uploaded)
 
-      headers['Content-Encoding'] = 'gzip'
-
-      for compiled_filename_gz in compiled_gz:
-        headers['Content-Type'] = (
-          mimetypes.guess_type(compiled_filename_gz)[0] or
-          'application/octet-stream'
-        )
-        files_gz_uploaded += self._upload(
-          tmpdir,
-          compiled_filename_gz,
-          bucket,
-          headers,
-          key_prefix,
-        )
-
-      logger.info(
-        "%r uploaded and %r gzip'd uploaded.",
-        files_uploaded,
-        files_gz_uploaded,
-      )
